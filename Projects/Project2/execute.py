3c92e68eebee730a89a94ef3b3e1e671396faa7c
SarahArmstrong <armstrong.sarah.kathleen@gmail.com>
2019-09-21 16:03:16 -0400

    Added parameter values

--- Projects/Project2/execute.py
+++ Projects/Project2/execute.py
@@ -9,16 +9,18 @@ period-lived overlapping generations model given some parameter values.
 # Set parameter values
 
 # Household parameters
-n1 = ?
-n2 = ?
-nvec = np.array([n1, n2])
-gamma = ?
-beta = ?
+n1       = 1.0
+n2       = 0.2
+nvec     = np.array([n1, n2])
+gamma    = 2.2
+beta_an  = 0.96
+beta     = beta_an ** (30) 
 
 # Firm parameters
-A = ?
-alpha = ?
-delta = ?
+A        = 1.0
+alpha    = 0.33
+delta_an = 0.05
+delta    = 1 - (1 - delta_an) ** (1/30)
 
 
 # Solve for b2 given parameters
